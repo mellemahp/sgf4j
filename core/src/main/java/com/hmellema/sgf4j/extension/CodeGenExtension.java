e77b072c1b53245d1af1dd306aa6a455926f741e
Hunter Mellema <mellemahp@gmail.com>
2023-01-03 15:04:22 -0700

    sync changes to codegen extension

---
+++ core/src/main/java/com/hmellema/sgf4j/extension/CodeGenExtension.java
@@ -0,0 +1,18 @@
+package com.hmellema.sgf4j.extension;
+
+
+import com.hmellema.sgf4j.mapping.Processor;
+import com.hmellema.sgf4j.mapping.Resolver;
+
+import java.util.List;
+
+public interface CodeGenExtension {
+    /** Unique name to assign to the extension
+     *
+     * @return Extension name
+     */
+    String getName();
+
+    List<Resolver> getResolvers();
+    List<Processor> getProcessors();
+}
